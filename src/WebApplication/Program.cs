45623f52860ab8fc5ecd9a81094a0d38c8102830
Vinny Martins <martins.vds@outlook.com>
2020-12-02 14:39:22 -0700

    add env variables config

--- src/WebApplication/Program.cs
+++ src/WebApplication/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace WebApplication
@@ -12,6 +13,10 @@ namespace WebApplication
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables(prefix: "Web_");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- src/WebApplication/Startup.cs
+++ src/WebApplication/Startup.cs
@@ -36,6 +36,7 @@ namespace WebApplication
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
             app.UseStaticFiles();
 
             app.UseRouting();
